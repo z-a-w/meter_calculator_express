37444f636e43605f81f8f28350a1fb2b3f22b076
Y2K222 <htetaungzaw811@gmail.com>
2022-11-28 12:55:56 +0630

    Updated

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const validator = require("express-validator");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-// TODO: import route here
-const meter_units = require('./routes/meter_units');
-const calculator = require('./routes/calculator');
+// Import routes here
+const meter_units = require("./routes/meter_units");
+const calculator = require("./routes/calculator");
 
 const app = express();
 
@@ -16,8 +16,8 @@ app.use(validator());
 
 app.use(function (req, res, next) {
   // res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Origin", "http://192.168.100.7:8080");
-  // res.set("Access-Control-Allow-Origin", "http://192.168.20.127:8080");
+  // res.set("Access-Control-Allow-Origin", "http://192.168.100.7:8080");
+  res.set("Access-Control-Allow-Origin", "http://192.168.1.10:8080");
   res.set("Access-Control-Allow-Credentials", "true");
   res.set(
     "Access-Control-Allow-Methods",
@@ -35,9 +35,13 @@ app.use("/api/calculator", calculator);
 
 // TODO: use route here
 app.listen(3000, () => {
-  console.log("=====================================================================\n")
+  console.log(
+    "=====================================================================\n"
+  );
   console.log(
     " < METER CALCULATOR API > \n Express server is running at port 3000 \n ready to go ! "
   );
-  console.log("=====================================================================\n")
+  console.log(
+    "=====================================================================\n"
+  );
 });
--- routes/calculator.js
+++ routes/calculator.js
@@ -1,86 +1,93 @@
-const mongojs = require('mongojs');
-const express = require('express');
+const mongojs = require("mongojs");
+const express = require("express");
+const { respondToUser, respondStatusToUser } = require("../helper");
 const router = express.Router();
-const db = mongojs('meter_calculator', ['calculator']);
+const db = mongojs("meter_calculator", ["calculator"]);
 
 // Get all calculators
 router.get("/", function (req, res) {
-    db.calculator.find({}, function (err, data) {
-        respondToUser(res, err, data);
-    });
+  db.calculator.find({}, function (err, data) {
+    respondToUser(res, err, data);
+  });
 });
 
 // Get one calculator
 router.get("/:id", function (req, res) {
-    let meterId = req.params.id;
-    req.checkParams("id", "invalid meterId").isMongoId();
-    let validation_errors = req.validationErrors();
-    if (validation_errors) {
-        res.status(400).json(validation_errors);
-        return false;
+  let meterId = req.params.id;
+  req.checkParams("id", "invalid meterId").isMongoId();
+  let validation_errors = req.validationErrors();
+  if (validation_errors) {
+    res.status(400).json(validation_errors);
+    return false;
+  }
+  db.calculator.findOne(
+    { _id: mongojs.ObjectId(meterId) },
+    function (err, data) {
+      respondToUser(res, err, data);
     }
-    db.calculator.findOne({ _id: mongojs.ObjectId(meterId) }, function (err, data) {
-        respondToUser(res, err, data);
-    });
+  );
 });
 
 // Create calculator
 router.post("/", function (req, res) {
-    req.checkBody("fromUnit", "From unit should be integer").isInt();
-    req.checkBody("toUnit", "To unit should be integer").isInt();
-    req.checkBody("price", "Price should be Inteber").isInt();
+  req.checkBody("fromUnit", "From unit should be integer").isInt();
+  req.checkBody("toUnit", "To unit should be integer").isInt();
+  req.checkBody("price", "Price should be Inteber").isInt();
 
-    var errors = req.validationErrors();
-    if (errors) {
-        res.status(400).json(errors);
-        return false;
-    }
+  var errors = req.validationErrors();
+  if (errors) {
+    res.status(400).json(errors);
+    return false;
+  }
 
-    req.body.createdAt = new Date();
-    req.body.updatedAt = new Date();
+  req.body.createdAt = new Date();
+  req.body.updatedAt = new Date();
 
-    db.calculator.insert(req.body, function (err, data) {
-        respondStatusToUser(res, err, data, { msg: 'Calculator added !' });
-    });
+  db.calculator.insert(req.body, function (err, data) {
+    respondStatusToUser(res, err, data, { msg: "Calculator added !" });
+  });
 });
 
 // Update a calculator
 router.put("/:id", function (req, res) {
-    let calculatorId = req.params.id;
-    req.checkParams("id", "Calculator Id should be mongoId").isMongoId();
-    req.checkBody("fromUnit", "From unit should be integer").isInt();
-    req.checkBody("toUnit", "To unit should be integer").isInt();
-    req.checkBody("price", "Price should be Inteber").isInt();
-    var errors = req.validationErrors();
-    if (errors) {
-        res.status(400).json(errors);
-        return false;
+  let calculatorId = req.params.id;
+  req.checkParams("id", "Calculator Id should be mongoId").isMongoId();
+  req.checkBody("fromUnit", "From unit should be integer").isInt();
+  req.checkBody("toUnit", "To unit should be integer").isInt();
+  req.checkBody("price", "Price should be Inteber").isInt();
+  var errors = req.validationErrors();
+  if (errors) {
+    res.status(400).json(errors);
+    return false;
+  }
+  req.body.updatedAt = new Date();
+  db.calculator.update(
+    { _id: mongojs.ObjectId(calculatorId) },
+    { $set: req.body },
+    { multi: false },
+    function (err, data) {
+      respondStatusToUser(res, err, data, { msg: "Calculator Updated !" });
     }
-    req.body.updatedAt = new Date();
-    db.calculator.update(
-        { _id: mongojs.ObjectId(calculatorId) },
-        { $set: req.body },
-        { multi: false },
-        function (err, data) {
-            respondStatusToUser(res, err, data, { msg: 'Calculator Updated !' })
-        }
-    );
+  );
 });
 
 // Delete meterunit
 router.delete("/:id", function (req, res) {
-    let calculatorId = req.params.id;
+  let calculatorId = req.params.id;
 
-    req.checkParams("id", "Calculator Id should be mongoId").isMongoId();
-    let validation_errors = req.validationErrors();
-    if (validation_errors) {
-        res.status(400).json(validation_errors);
-        return false;
+  req.checkParams("id", "Calculator Id should be mongoId").isMongoId();
+  let validation_errors = req.validationErrors();
+  if (validation_errors) {
+    res.status(400).json(validation_errors);
+    return false;
+  }
+
+  db.calculator.remove(
+    { _id: mongojs.ObjectId(calculatorId) },
+    function (err, data) {
+      respondStatusToUser(res, err, data, { msg: "Calculator Deleted !" });
     }
-
-    db.calculator.remove({ _id: mongojs.ObjectId(calculatorId) }, function (err, data) {
-        respondStatusToUser(res, err, data, { msg: 'Calculator Deleted !' })
-    });
+  );
 });
 
 module.exports = router;
